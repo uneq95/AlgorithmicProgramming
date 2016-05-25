2124b424551d7e5a8313b4e02e5bb01f42064c6f
Ritesh Kumar <ritesh.kumar.ece13@iitbhu.ac.in>
2016-05-25 17:55:23 +0530

    coin-change AC

--- hackerrank/domains/algorithms/dynamic-programming/coin-change.cpp
+++ hackerrank/domains/algorithms/dynamic-programming/coin-change.cpp
@@ -1,57 +1,50 @@
 #include<iostream>
-#include<algorithm>
+#include<algorithm>//for sort()
 using namespace std;
 
+long num_of_change_ways(int n, int den[],int m){
+    //the array den is used to store the denominations available for change
+    //n = amount to be changed
+    //m = number of denominations
+    sort(den,den+m);
+    long a[51][251]={0};//the array a is used to store the number of ways of changing amount i
+    for(int i=1;i<=m;i++){
+        a[i][0]=1;
+    }
+
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+
+            if(den[i-1]<=j){
+                a[i][j]=a[i-1][j]+a[i][j-den[i-1]];
+            }else{
+                a[i][j]=a[i-1][j];
+            }
+        }
+    }
+    /*for(int x = 0 ;x <= m ; x++){
+        cout<<endl;
+        for(int y= 0;y<=n;y++){
+            cout<<a[x][y]<<"\t";
+        }
+    }
+    cout<<endl;*/
+    return a[m][n];
+
+}
 int main(){
 
     int n;//total amount to be changed
     int m;//number of denominations available
-    //n=1;
-    //m=25;
     cin>>n>>m;
     int den[m];//the denominations available for change
-    //int s[] ={5 ,37, 8, 39, 33, 17, 22, 32, 13, 7 ,10, 35, 40 ,2 ,43, 49, 46, 19, 41, 1, 12, 11 ,28 };
     for(int i=0;i<m;i++)
-        cin>>den[m];//den[i]=s[i];
+        cin>>den[i];
 
-    int a[51][251]={0};
-
-    for(int i=0;i<=m;i++){
-        a[i][0]=1;
-    }
-    for(int x = 0 ;x <= m ; x++){
-                    cout<<endl;
-                for(int y= 0;y<=n;y++){
-                    cout<<a[x][y]<<"\t";
-                }
-            }
-        cout<<endl;
-    for(int j=1;j<=m;j++){
-        for(int i=1;i<=n;i++){
-
-            if(den[j-1]<=i){
-                a[j][i]=a[j-1][i]+a[j][i-den[j-1]];
-            }else{
-                a[j][i]=a[j-1][i];
-            }
-
-            for(int x = 0 ;x <= m ; x++){
-                    cout<<endl;
-                for(int y= 0;y<=n;y++){
-                    cout<<a[x][y]<<"\t";
-                }
-            }
-        cout<<endl;
-        }
-        //cout<<endl<<endl;
-    }
-    cout<<a[m+1][n+1];
-    /*int sum=0;
-    for(int i = 0; i <= n; i++){
-        for(int j = 0 ; j <= m; j ++){
-            sum = sum + a[i][j];
-        }
-    }
-    cout<<sum;*/
+    long ways = num_of_change_ways(n,den,m);
+    //the number of ways can be a large number which could not be stored in int variable
+    //hence long type has been used here
+    //using int type 3 test cases gave wrong answer
+    cout<<ways;
     return 0;
 }
