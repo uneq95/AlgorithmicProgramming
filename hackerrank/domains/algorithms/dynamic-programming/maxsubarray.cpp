32b8a08a7730471e23c251d7641ab2a9c098e5f1
Ritesh Kumar <ritesh.kumar.ece13@iitbhu.ac.in>
2016-05-25 11:37:49 +0530

    maxsubarray comments added

--- hackerrank/domains/algorithms/dynamic-programming/maxsubarray.cpp
+++ hackerrank/domains/algorithms/dynamic-programming/maxsubarray.cpp
@@ -3,14 +3,26 @@
 #include<climits>// for using INT_MIN
 using namespace std;
 
+//refer here
+//https://www.youtube.com/watch?v=hXlv88ddcgw
 
 int max_subsequence_sum(int a[],int n){
+    //just sort the array in increasing order
+    //if the array contains positive numbers then take the sum of
+    //positive numbers from the end of the array
+
+    //if the number does not contain positive number then
+    //take the first negative number as the max subsequence sum for obvious reasons
 
     int sum =0;
     sort(a,a+n);
     for(int i=n-1;i>=0;i--){
 
         if(a[i]<0){
+                //this if(i==n-1) condition is to check whether the array contains a negative number
+                //as the last element(or the biggest element)
+                //because if the array does not contain a zero at the last index then it will have a
+                // negative number at the last index, which will be the maximum sum subsequence
                 if(i==n-1)
                     sum=sum+a[i];
 
